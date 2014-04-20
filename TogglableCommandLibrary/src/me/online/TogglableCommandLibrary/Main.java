68f084bb9481f75373ce6500463f6b3a7436219d
Kyle <kyoungnumber4@gmail.com>
2014-04-20 16:35:11 -0700

    First edit

--- TogglableCommandLibrary/src/me/online/TogglableCommandLibrary/Main.java
+++ TogglableCommandLibrary/src/me/online/TogglableCommandLibrary/Main.java
@@ -1,5 +1,16 @@
 package me.online.TogglableCommandLibrary;
 
-public class Main {
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener{
+	public void onEnable() {
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
+		getConfig().options().copyDefaults(true);
+		getConfig().options().header("Configuration for Togglable Command Library");
+		getConfig().options().copyHeader(true);
+		saveConfig();
+	}
 
 }
